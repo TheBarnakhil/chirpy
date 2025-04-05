6da87d9b1cc11af0bec24b90d26cfc9d4678c993
TheBarnakhil <nsakhil763@gmail.com>
2025-04-05 17:51:47 +0530

    Added in a custom middleware

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Add(1) // Increment the counter
+		next.ServeHTTP(rw, req)   // Call the next handler
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	serveMux := http.NewServeMux()
+	apiCfg := &apiConfig{fileServerHits: atomic.Int32{}}
 
 	fileServer := http.FileServer(http.Dir(filepathRoot))
-	serveMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 	serveMux.HandleFunc("/healthz", healthHandler)
+	serveMux.HandleFunc("/metrics", apiCfg.hitsHander)
+	serveMux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Handler: serveMux,
@@ -23,9 +39,17 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func healthHandler(rw http.ResponseWriter, req *http.Request) {
+func (cfg *apiConfig) hitsHander(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	rw.WriteHeader(200)
-	rw.Write([]byte("OK"))
+	rw.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(rw http.ResponseWriter, req *http.Request) {
+	cfg.fileServerHits.Store(0)
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	rw.WriteHeader(200)
+	rw.Write([]byte("Reset the hits"))
 }
---
+++ readiness.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func healthHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	rw.WriteHeader(200)
+	rw.Write([]byte("OK"))
+}
