33be03f63ba0eb37abb9d3c376608f1076a03af4
TheBarnakhil <nsakhil763@gmail.com>
2025-04-05 16:04:15 +0530

    Added app prefixed routes for handling fileserver and a handler function for health endpoint

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func main() {
 	serveMux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir(filepathRoot))
-	serveMux.Handle("/", fileServer)
+	serveMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	serveMux.HandleFunc("/healthz", healthHandler)
 
 	server := &http.Server{
 		Handler: serveMux,
@@ -20,4 +21,11 @@ func main() {
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	rw.WriteHeader(200)
+	rw.Write([]byte("OK"))
 }
